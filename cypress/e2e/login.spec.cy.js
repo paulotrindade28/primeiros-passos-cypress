0e9ab9755b3f738c39c16c892a25da6ef6247a8f
Paulo Trindade <pauloastrindade@gmail.com>
2024-06-25 09:52:54 -0300

    added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,18 +1,28 @@
 describe('Orange HRM Tests', () => { //.skip - travar sua pesquisa.
+
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButoon: "[type='submit']",
+    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+    wrongCredentilAlert: "[role='alert']"
+
+  }
+
   it('Login - Success', () => { //teste
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('ADMIN')//usuário/seletores/comando
-    cy.get("[name='password']").type('admin123') //senha/seletores/comando
-    cy.get("[type='submit']").click() //botao - entrer
+    cy.get(selectorsList.usernameField).type('ADMIN')//usuário/seletores/comando
+    cy.get(selectorsList.passwordField).type('admin123') //senha/seletores/comando
+    cy.get(selectorsList.loginButoon).click() //botao - entrer
     cy.location('pathname').should('equal',"/web/index.php/dashboard/index")// checar - igual
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains("Dashboard") // get- checar se existe
+    cy.get(selectorsList.sectionTitleTopBar).contains("Dashboard") // get- checar se existe
   })
   it('Login - Fail', () => { 
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('test')//ADMIN
-    cy.get("[name='password']").type('ateste3') //dmin123
-    cy.get("[type='submit']").click() 
-    cy.get("[role='alert']") //classe - nunca teste o texto
+    cy.get(selectorsList.usernameField).type('test')//ADMIN
+    cy.get(selectorsList.passwordField).type('ateste3') //dmin123
+    cy.get(selectorsList.loginButoon).click() 
+    cy.get(selectorsList.wrongCredentilAlert) //classe - nunca teste o texto
   })
     
 })
